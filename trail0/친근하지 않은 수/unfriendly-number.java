b1171c45491252a9499fb9d13ac9a8521a15cdd9
트시무 <130881888+Tsimoo07@users.noreply.github.com>
2026-06-29 19:13:50 +0900

    [Passed] 친근하지 않은 수 (133ms, 10MB)

---
+++ trail0/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        for (int i=1; i<=n; i++) {
+            if (i%2!=0 && i%3!=0 && i%5!=0) {c += 1;}
+        }
+        System.out.println(c);
+    }
+}
