10f462e3041c94e1aa8e2ce09833e0d3956295fb
트시무 <130881888+Tsimoo07@users.noreply.github.com>
2026-07-01 17:19:27 +0900

    [Passed] 배열의 합 (129ms, 10MB)

---
+++ trail0/배열의 합/sum-of-array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<4; i++) {
+            int tot = 0;
+            for (int j=0; j<4; j++) {
+                tot += sc.nextInt();
+            }
+            System.out.println(tot);
+        }
+    }
+}
